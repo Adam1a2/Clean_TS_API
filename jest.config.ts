0d6578c353bacbd13b8eaa78940f24aba90e6ad5
Adam1a2 <adamhenrique67@gmail.com>
2022-03-16 12:14:20 -0300

    refactor: changing the type of jest config files to ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,14 @@
-module.exports = {
+export default {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts', '**/*.test.ts'],
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  }
+  },
+  watchPathIgnorePatterns: ['globalConfig']
 }
