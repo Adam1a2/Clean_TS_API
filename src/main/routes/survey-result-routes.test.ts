44b00c83a7d7d768ab386f51d55b814c029d19fb
Adam1a2 <adamhenrique67@gmail.com>
2022-04-28 22:18:09 -0300

    test: ensure save survey result route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,6 +1,30 @@
 import request from 'supertest'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
+import { Collection } from 'mongodb'
+
+let surveyCollection: Collection
+let accountCollection: Collection
+
+const makeAccessToken = async (): Promise<string> => {
+  const result = await accountCollection.insertOne({
+    name: 'Adam',
+    email: 'adamhenrique67@gmail.com',
+    password: '123'
+  })
+  const { insertedId: id } = result
+  const accessToken = sign({ id }, env.jwtSecret)
+  await accountCollection.updateOne({
+    _id: id
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+  return accessToken
+}
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -9,6 +33,13 @@ describe('Survey Routes', () => {
 
   afterAll(async () => {
     await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('PUT  /surveys/:surveyId/results', () => {
@@ -21,6 +52,26 @@ describe('Survey Routes', () => {
         .expect(403)
     })
 
+    test('Should return 200 on save survey result with accessToken', async () => {
+      const accessToken = await makeAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+      await request(app)
+        .put(`/api/surveys/${res.insertedId}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'Answer1'
+        })
+        .expect(200)
+    })
   
   })
 
