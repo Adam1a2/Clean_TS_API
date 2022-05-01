a0741199cc52b91a72906c1b23870082d0434a37
Adam1a2 <adamhenrique67@gmail.com>
2022-05-01 19:07:14 -0300

    test: ensure LoadSurveyResultController returns 200 on success

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -1,8 +1,9 @@
 import { HttpRequest, LoadSurveyById, mockLoadSurveyById, mockLoadSurveyResult, LoadSurveyResult } from "./load-survey-result-controller-protocols"
 import { LoadSurveyResultController } from "./load-survey-result-controller"
 import { InvalidParamError } from "@/presentation/errors"
-import { forbidden, serverError } from "@/presentation/helpers/http/http-helper"
-import { throwError } from "@/domain/test"
+import { forbidden, serverError, ok } from "@/presentation/helpers/http/http-helper"
+import { mockSurveyResultModel, throwError } from "@/domain/test"
+import MockDate from 'mockdate'
 
 const makeFakeRequest = (): HttpRequest => ({
   params: {
@@ -28,6 +29,13 @@ const makeSut = (): SutTypes => {
 }
 
 describe('LoadSurveyResult Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
   test('Should call LoadSurveyById with correct values', async() => {
     const { sut, loadSurveyByIdStub } = makeSut()
     const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, "loadById")
@@ -59,7 +67,13 @@ describe('LoadSurveyResult Controller', () => {
   test('Should return 500 if LoadSurveyResult throws', async () => {
     const { sut, loadSurveyResultStub } = makeSut()
     jest.spyOn(loadSurveyResultStub, 'load').mockImplementationOnce(throwError)
-    const httpResponse = await sut.handle({})
+    const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(ok(mockSurveyResultModel()))
   })
 })
